54998746c99e346bb06b9226cef1baa2cc884f53
Huy <thanhhuydev77@gmail.com>
2020-04-02 22:35:54 +0700

    test cors 2

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"ROOMS/CONTROLLERS"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"net/http"
 )
 
 func main() {
 	r := mux.NewRouter()
 	CONTROLLERS.InitAllController(r)
-	http.ListenAndServe(":8080", r)
+	handler := cors.Default().Handler(r)
+	http.ListenAndServe(":8080", handler)
 }
