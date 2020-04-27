0cabfb0040e8c00eefea34b171cf195c260bcd9d
Huy <thanhhuydev77@gmail.com>
2020-04-27 21:47:54 +0700

    change response message to adapt FE Dev request

--- CONTROLLERS/UPLOADFILE.go
+++ CONTROLLERS/UPLOADFILE.go
@@ -1,27 +1,38 @@
 package CONTROLLERS
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func UploadPicture(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		fmt.Println(err)
+		io.WriteString(w, `{"message":"Can’t upload avatar"}`)
 		return
 	}
 	defer file.Close()
 
-	fmt.Fprintf(w, "%v", handler.Header)
 	f, err := os.OpenFile("./ASSET/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println(err)
+		io.WriteString(w, `{"message":"Can’t upload avatar"}`)
 		return
 	}
 	defer f.Close()
-
 	io.Copy(f, file)
+	io.WriteString(w, `{ "status": 200,
+    "message": "Upload avatar success",
+    "data": {
+        "fieldname": "file",
+        "originalname": "`+handler.Filename+`",
+        "destination": "ASSET",
+		 "mimetype": "`+handler.Header.Get("Content-Type")+`",
+        "filename": "`+handler.Filename+`",
+        "path": "ASSET\\`+handler.Filename+`",
+        "size": `+strconv.Itoa(int(handler.Size))+`
+    }
+}`)
 }
