a186f5e02b604436da0f7e167923d76cf5e3caef
Truong vi <vinlt@antsprogrammatic.com>
2020-07-07 15:07:50 +0700

    Update Dockerfile

--- main.go
+++ main.go
@@ -19,11 +19,11 @@ func main() {
 		Db: DATABASE.GetDbInstance(),
 	}
 	defer app.Db.Close()
-	fmt.Print("Server is running at port 8001...")
+	fmt.Print("Server is running at port 8103...")
 	InitAllController(*app, r, Redis)
 	//allow all method CORS
 	handler := cors.AllowAll().Handler(r)
-	http.ListenAndServe(":8001", handler)
+	http.ListenAndServe(":8103", handler)
 
 }
 func NewRouter() *mux.Router {
